b299669e0582132d9460c5f8f0a8bec37e276c60
Vitaly Dragun <vdrag00n90@gmail.com>
2018-02-18 15:43:04 +0200

    Repair mongdb url

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: 'mongodb://vitaly1990:199002225@ds141028.mlab.com:41028/vidjot-prod'
+        mongoURI: 'mongodb://vitaly1990:19900225@ds141028.mlab.com:41028/vidjot-prod'
     }
 } else {
     module.exports = {
